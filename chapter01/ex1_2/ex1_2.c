d22ff218dc97d2910754eb08fc759e73f84bb551
kyo1941 <kyo1941@outlook.jp>
2023-11-24 00:07:27 +0900

    style: フォーマットの訂正

--- chapter01/ex1_2/ex1_2.c
+++ chapter01/ex1_2/ex1_2.c
@@ -6,12 +6,12 @@ int main(void)
 {
   STEREO_PCM pcm0, pcm1;
   int n;
-  
+
   stereo_wave_read(&pcm0, "c.wav"); /* WAVEファイルからモノラルの音データを入力する */
-  
-  pcm1.fs = pcm0.fs; /* 標本化周波数 */
-  pcm1.bits = pcm0.bits; /* 量子化精度 */
-  pcm1.length = pcm0.length; /* 音データの長さ */
+
+  pcm1.fs = pcm0.fs;                             /* 標本化周波数 */
+  pcm1.bits = pcm0.bits;                         /* 量子化精度 */
+  pcm1.length = pcm0.length;                     /* 音データの長さ */
   pcm1.sL = calloc(pcm1.length, sizeof(double)); /* メモリの確保 */
   pcm1.sR = calloc(pcm1.length, sizeof(double)); /* メモリの確保 */
   for (n = 0; n < pcm1.length; n++)
@@ -21,11 +21,11 @@ int main(void)
   }
 
   stereo_wave_write(&pcm1, "d.wav"); /* WAVEファイルにモノラルの音データを出力する */
-  
+
   free(pcm0.sL); /* メモリの確保 */
   free(pcm0.sR); /* メモリの確保 */
   free(pcm1.sL); /* メモリの確保 */
   free(pcm1.sR); /* メモリの確保 */
-  
+
   return 0;
 }
