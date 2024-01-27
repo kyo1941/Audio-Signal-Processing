2f00959f1eb9d1ae5cefa395ec32c8940e20c09f
kyo1941 <138639762+kyo1941@users.noreply.github.com>
2024-01-27 16:34:16 +0000

    feat: chapter01やった

--- chapter01/ex1_2/ex1_2.c
+++ chapter01/ex1_2/ex1_2.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "wave.h"
 
-int main(void)
-{
+int main(void) {
   STEREO_PCM pcm0, pcm1;
   int n;
 
-  stereo_wave_read(&pcm0, "c.wav"); /* WAVEファイルからモノラルの音データを入力する */
+  stereo_wave_read(&pcm0,
+                   "c.wav"); /* WAVEファイルからモノラルの音データを入力する */
 
   pcm1.fs = pcm0.fs;                             /* 標本化周波数 */
   pcm1.bits = pcm0.bits;                         /* 量子化精度 */
   pcm1.length = pcm0.length;                     /* 音データの長さ */
   pcm1.sL = calloc(pcm1.length, sizeof(double)); /* メモリの確保 */
   pcm1.sR = calloc(pcm1.length, sizeof(double)); /* メモリの確保 */
-  for (n = 0; n < pcm1.length; n++)
-  {
+  for (n = 0; n < pcm1.length; n++) {
     pcm1.sL[n] = pcm0.sL[n]; /* 音データのコピー */
     pcm1.sR[n] = pcm0.sR[n]; /* 音データのコピー */
   }
 
-  stereo_wave_write(&pcm1, "d.wav"); /* WAVEファイルにモノラルの音データを出力する */
+  stereo_wave_write(&pcm1,
+                    "d.wav"); /* WAVEファイルにモノラルの音データを出力する */
 
   free(pcm0.sL); /* メモリの確保 */
   free(pcm0.sR); /* メモリの確保 */
