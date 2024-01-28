7296b8aebbe395b9a985adb32e515b09393f9ff8
kyo1941 <kyo1941@outlook.jp>
2024-01-28 21:18:56 +0900

    style: フォーマット

--- chapter06/ex6_4/ex6_4.c
+++ chapter06/ex6_4/ex6_4.c
@@ -1,20 +1,21 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include "wave.h"
-#include "iir_filter.h"
 
-int main(void)
-{
+#include "iir_filter.h"
+#include "wave.h"
+
+int main(void) {
   MONO_PCM pcm0, pcm1;
   int n, m, I, J, L, offset, frame, number_of_frame;
   double fc, Q, a[3], b[3], *x, *y;
-  
-  mono_wave_read(&pcm0, "sample04.wav"); /* WAVEファイルにモノラルの音データを入力する */
-  
-  pcm1.fs = pcm0.fs; /* 標本化周波数 */
-  pcm1.bits = pcm0.bits; /* 量子化精度 */
-  pcm1.length = pcm0.length; /* 音データの長さ */
+
+  mono_wave_read(
+      &pcm0, "sample04.wav"); /* WAVEファイルにモノラルの音データを入力する */
+
+  pcm1.fs = pcm0.fs;                            /* 標本化周波数 */
+  pcm1.bits = pcm0.bits;                        /* 量子化精度 */
+  pcm1.length = pcm0.length;                    /* 音データの長さ */
   pcm1.s = calloc(pcm1.length, sizeof(double)); /* メモリの確保 */
 
   fc = 1000.0 / pcm0.fs; /* 遮断周波数 */
@@ -23,68 +24,58 @@ int main(void)
   J = 2;                 /* 遅延期の数 */
 
   IIR_LPF(fc, Q, a, b); /* IIRフィルタの設計 */
-  
+
   L = 256; /* フレームの長さ */
-  
+
   number_of_frame = pcm0.length / L; /* フレームの数 */
-  
+
   x = calloc((L + J), sizeof(double)); /* メモリの確保 */
   y = calloc((L + I), sizeof(double)); /* メモリの確保 */
-  
-  for (frame = 0; frame < number_of_frame; frame++)
-  {
+
+  for (frame = 0; frame < number_of_frame; frame++) {
     offset = L * frame;
-    
-    /* ���O�̃t���[���̌㔼��J�T���v������������ */
-    for (n = 0; n < L + J; n++)
-    {
-      if (offset - J + n < 0)
-      {
+
+    /* ���O�̃t���[���̌㔼��J�T���v������������
+     */
+    for (n = 0; n < L + J; n++) {
+      if (offset - J + n < 0) {
         x[n] = 0.0;
-      }
-      else
-      {
+      } else {
         x[n] = pcm0.s[offset - J + n];
       }
     }
-    
-    /* ���O�̃t�B���^�����O���ʂ̌㔼��I�T���v������������ */
-    for (n = 0; n < L + I; n++)
-    {
-      if (offset - I + n < 0)
-      {
+
+    /* ���O�̃t�B���^�����O���ʂ̌㔼��I�T���v������������
+     */
+    for (n = 0; n < L + I; n++) {
+      if (offset - I + n < 0) {
         y[n] = 0.0;
-      }
-      else
-      {
+      } else {
         y[n] = pcm1.s[offset - I + n];
       }
     }
-    
+
     /* フィルタリング */
-    for (n = 0; n < L; n++)
-    {
-      for (m = 0; m <= J; m++)
-      {
+    for (n = 0; n < L; n++) {
+      for (m = 0; m <= J; m++) {
         y[I + n] += b[m] * x[J + n - m];
       }
-      for (m = 1; m <= I; m++)
-      {
+      for (m = 1; m <= I; m++) {
         y[I + n] += -a[m] * y[I + n - m];
       }
     }
-    
+
     /* フィルタリング���ʂ̘A�� */
-    for (n = 0; n < L; n++)
-    {
+    for (n = 0; n < L; n++) {
       pcm1.s[offset + n] = y[I + n];
     }
   }
-  
-  mono_wave_write(&pcm1, "ex6_4.wav"); /* WAVEファイルにモノラルの音データを出力する */
-  
+
+  mono_wave_write(&pcm1,
+                  "ex6_4.wav"); /* WAVEファイルにモノラルの音データを出力する */
+
   free(pcm0.s); /* メモリの解放 */
   free(pcm1.s); /* メモリの解放 */
-  
+
   return 0;
 }
